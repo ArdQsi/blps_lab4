b358c8f7b6c6ac7916b3ae4b612d6a6352183027
Denstep <denvvbaranov@gmail.com>
2024-05-15 04:10:51 +0300

    update allfilms

--- src/main/java/com/webapp/delegators/AllFilms.java
+++ src/main/java/com/webapp/delegators/AllFilms.java
@@ -1,10 +1,13 @@
 package com.webapp.delegators;
 
+import com.webapp.dto.FilmDto;
 import com.webapp.service.FilmService;
 import jakarta.inject.Named;
 import lombok.RequiredArgsConstructor;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import java.util.List;
 
 @Named("allfilms")
 @RequiredArgsConstructor
@@ -14,6 +17,11 @@ public class AllFilms implements JavaDelegate {
 
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
-        delegateExecution.setVariable("films", filmService.getAllFilm().toString());
+        List<FilmDto> films = filmService.getAllFilm();
+        int i = 0;
+        for (FilmDto film : films) {
+            delegateExecution.setVariable("film "+i, film.getName());
+            i++;
+        }
     }
 }
